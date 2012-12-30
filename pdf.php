3cbccc1ca0b321c6c093be2c6af42d67dab7abe7
Blue Liquid Designs <Blue Liquid Designs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-30 22:46:25 +0000

    WP3.5 Update
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-pdf-extended/tags/1.2.3@646189 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pdf.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+Plugin Name: Gravity Forms PDF Extended
+Plugin URI: http://www.blueliquiddesigns.com.au/index.php/gravity-forms-pdf-extended-plugin/
+Description: Gravity Forms PDF Extended allows you to save/view/download a PDF from the front- and back-end, and automate PDF creation on form submission. Note: If you're upgrading, backup your plugin files beforehand as your custom template will be overridden.
+Version: 1.2.2
+Author: Blue Liquid Designs
+Author URI: http://www.blueliquiddesigns.com.au
+
+------------------------------------------------------------------------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+*/
+
+/*
+ * Handles the admin area routing 
+ * See render_to_pdf.php for PDF Output Functions
+ */
+
+add_action('gform_entries_first_column_actions', 'pdf_link', 10, 4);
+add_action("gform_entry_info", "detail_pdf_link", 10, 2);
+add_action('wp',   'process_exterior_pages');
+register_activation_hook( __FILE__, 'pdf_extended_activate' );
+
+
+add_action('admin_init',  'gfe_admin_init', 9);
+add_action("gform_entry_created", "gform_pdf_example_create", 10, 2);
+add_filter("gform_admin_notification_attachments", 'gform_add_example_attachment', 10, 3);
+
+
+/*
+ * Generate our Sample PDF for our Sample Form   
+ */
+function gform_pdf_example_create($entry, $form)
+{
+	$user_id = $entry['id'];
+	$form_id = $entry['form_id'];
+		
+	if($form['title'] == 'Gravity Forms PDF Extended Custom Template Sample')
+	{
+		/* include the pdf processing file */
+		require ABSPATH. 'wp-content/plugins/gravity-forms-pdf-extended/render_to_pdf.php';
+                /* generate and save the PDF file*/
+		$filename = PDF_Generator($form_id, $user_id, 'save', true, 'example-template.php');	
+	}
+}
+
+/* Emails our sample PDF to the site administrator */
+function gform_add_example_attachment($attachments, $lead, $form){
+ 
+	$form_id = $lead['form_id'];
+	$user_id = $lead['id'];
+	$attachments = array();
+ 
+	if($form['title'] == 'Gravity Forms PDF Extended Custom Template Sample')
+	{
+		/* include PDF converter plugin */
+		include ABSPATH. 'wp-content/plugins/gravity-forms-pdf-extended/render_to_pdf.php';
+		$attachment_file = PDF_SAVE_LOCATION. get_pdf_filename($form_id, $user_id);
+		$attachments[] = $attachment_file;
+		return $attachments;
+	}  
+}
+
+/**
+ * Check to see if Gravity Forms is actually installed
+ */
+function gfe_admin_init()
+{
+	if(!class_exists("RGForms"))
+	{
+		/* throw error to the admin notice bar */
+		add_action('admin_notices', 'gf_not_installed'); 	
+	}
+	
+	if(get_option('gf_pdf_extended_sample') != 'installed')
+	{
+		pdf_extended_activate();
+	}
+}
+
+/**
+ * Install an example form to show off the new template system
+ */
+function pdf_extended_activate()
+{
+	GFExport::import_file(ABSPATH .'/wp-content/plugins/gravity-forms-pdf-extended/example-form.xml');
+	update_option('gf_pdf_extended_sample', 'installed');
+}
+
+/**
+ * Gravity Forms hasn't been installed so throw error.
+ * We make sure the user hasn't already dismissed the error
+ */
+function gf_not_installed()
+{
+	global $current_user;
+    $user_id = $current_user->ID;
+    
+	/* Check that the user hasn't already clicked to ignore the message */
+    if ( ! get_user_meta($user_id, 'gfpdfe_ignore_notice') ) {
+		// Shows as an error message. You could add a link to the right page if you wanted.
+		echo '<div id="message" class="error"><p>';
+		printf(__('You need to install <a href="http://www.gravityforms.com/">Gravity Forms</a> to use the Gravity Forms PDF Extended Plugin. | <a href="%1$s">Hide Notice</a>'), '?gfpdfe_nag_ignore=1');
+		echo '</p></div>';
+	}
+}
+
+/**
+ * Gravity Forms hasn't been installed and user is dismissing the error thrown
+ */
+add_action('admin_init', 'gfpdfe_nag_ignore');
+function gfpdfe_nag_ignore() {
+    global $current_user;
+    $user_id = $current_user->ID;
+    /* If user clicks to ignore the notice, add that to their user meta */
+    if ( isset($_GET['gfpdfe_nag_ignore']) && $_GET['gfpdfe_nag_ignore'] == 1 ) {
+          add_user_meta($user_id, 'gfpdfe_ignore_notice', 'true', true);
+    }
+}
+
+//Link for Entry Detail View (Provide both View Link and Download)
+function detail_pdf_link($form_id, $lead) {
+  $lead_id = $lead['id'];
+  echo "PDF:  ";
+  echo "<a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> View</a>";
+  echo " <a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&download=1&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> Download</a>";
+}
+
+// Made this first... figured i would leave it in.  View link on the Entry list view. 
+function pdf_link($form_id, $field_id, $value, $lead) {
+  $lead_id = $lead['id'];
+  echo "| <a href=\"javascript:;\" onclick=\"var notes_qs = '&notes=1'; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\"> View PDF</a>";
+}
+
+//Handle Incoming route.   Look for GF_PDF namespace 
+function process_exterior_pages(){
+  global $wpdb;
+  
+  if(rgempty("gf_pdf", $_GET))
+    return;
+    
+	$form_id = $_GET['fid'];
+	$lead_id = $_GET['lid'];
+	$ip = $_GET['ip'];
+	$template = (rgempty('template', $_GET)) ? 'pdf-print-entry.php' : rgget('template');
+	
+	/* check the lead is in the database and the IP address matches (little security booster) */
+	$form_entries = $wpdb->get_var( $wpdb->prepare("SELECT count(*) FROM `".$wpdb->prefix."rg_lead` WHERE form_id = ".$form_id." AND status = 'active' AND id = ".$lead_id." AND ip = '".$ip."'", array() ) )	;	
+
+  //ensure users are logged in
+  if(!is_user_logged_in() && !rgempty('template', $_GET) && $form_entries == 0)
+    auth_redirect();
+
+  switch(rgget("gf_pdf")){
+    case "print-entry" :
+    require_once("render_to_pdf.php");
+	/* call the creation class */
+	$output = ($_GET['download'] == 1) ? 'download' : 'view';
+	PDF_Generator((int) $_GET['fid'], (int) $_GET['lid'], $output, false, $template);
+    break;
+  }
+  exit();
+}
+
+
+
+?>
---
+++ render_to_pdf.php
@@ -0,0 +1,141 @@
+<?php
+// Handy trick to let us run the default print view and give us the processed string... it's all we want really. 
+if (!function_exists('GetRequire')) {
+	function GetRequire($sFilename) {
+	  ob_start();
+	  require($sFilename);
+	  $sResult = ob_get_contents();
+	  ob_end_clean();
+	  return $sResult;
+	  return ob_get_clean();
+	}
+}
+
+define('PDF_SAVE_LOCATION', dirname(__FILE__)."/output/");
+
+/**
+ * Outputs a PDF entry from a Gravity Form
+ * var $form_id integer: The form id
+ * var $lead_id integer: The entry id
+ * var $output string: either view, save or download
+ * save will save a copy of the PDF to the server using the PDF_SAVE_LOCATION constant
+ * var $return boolean: if set to true it will return the path of the saved PDF
+ * var $template string: if you want to use multiple PDF templates - name of the template file
+ */
+ if (!function_exists('PDF_Generator')) {
+	function PDF_Generator($form_id, $lead_id, $output = 'view', $return = false, $template = 'pdf-print-entry.php')
+	{
+		$filename = get_pdf_filename($form_id, $lead_id); 
+		$entry = load_entry_data($form_id, $lead_id, $template);	
+		PDF_processing($entry, $filename, $output);
+		/* return the filename so we can use it */
+		if($return)
+		{
+			return PDF_SAVE_LOCATION. $filename;
+		}
+	}
+ }
+
+/**
+ * Get the name of the PDF based on the Form and the submission
+ */
+ if (!function_exists('get_pdf_filename')) {
+	function get_pdf_filename($form_id, $lead_id)
+	{
+		return "form-$form_id-entry-$lead_id.pdf";
+	}
+ }
+
+/**
+ * Loads the Gravity Form output script (actually the print preview)
+ */
+ if (!function_exists('load_entry_data')) {
+	function load_entry_data($form_id, $lead_id, $template)
+	{
+		/* set up contstants for gravity forms to use so we can override the security on the printed version */
+		define('GF_FORM_ID', $form_id);
+		define('GF_LEAD_ID', $lead_id);	
+		return GetRequire(dirname(__FILE__)."/templates/" . $template);
+	}
+ }
+
+/**
+ * Creates the PDF and does a specific output (see PDF_Generator function above for $output variable types)
+ */
+ if (!function_exists('PDF_processing')) {
+	function PDF_processing($entry, $filename, $output = 'view')
+	{
+		//Parse the default print view from Gravity forms so we can play with it.
+		$DOM = new DOMDocument;
+		$DOM->loadHTML($entry);
+		
+		//Make Stylesheets/Images Absolute
+		/*$stylesheets = $DOM->getElementsByTagName('link');
+		
+		foreach($stylesheets as $stylesheet){
+		  $href = $stylesheet->getAttribute('href');
+		  if(strpos($src, site_url()) !== 0){
+			$stylesheet->setAttribute('href', site_url()."$href");
+		  }
+		}*/
+		
+		/* Problem pulling the site style so manually set */
+		
+		$imgs = $DOM->getElementsByTagName('img');
+		foreach($imgs as $img){
+		  $src = $img->getAttribute('src');
+		  if(strpos($src, site_url()) !== 0){
+			$img->setAttribute('src', site_url()."$src");
+		  }
+		}
+		
+		//Remove Ugly Header
+		$xpath = new DOMXPath($DOM);
+		$nlist = $xpath->query("//div[@id='print_preview_hdr']");
+		$node = $nlist->item(0);
+		$node->parentNode->removeChild($node);
+		$entry = $DOM->saveHTML();
+		
+		//Load the DOMPDF Engine to render the PDF
+		require_once("dompdf/dompdf_config.inc.php");
+		$dompdf = new DOMPDF();
+		$dompdf -> load_html($entry);
+		$dompdf -> set_base_path(site_url());
+		$dompdf -> render();	
+		
+		switch($output)
+		{
+			case 'download':
+				 $dompdf -> stream($filename, array("Attachment" => true));
+				 exit(0);
+			break;
+			
+			case 'view':
+				 $dompdf -> stream($filename, array("Attachment" => false));
+				 exit(0);
+			break;
+			
+			case 'save':
+				savePDF($dompdf, $filename);
+			break;
+		}
+	}
+ }
+
+/**
+ * Creates the PDF and does a specific output (see PDF_Generator function above for $output variable types)
+ * var $dompdf Object
+ */
+  if (!function_exists('savePDF')) {
+	function savePDF($dompdf, $filename) 
+	{
+		$pdf = $dompdf->output();
+		if(!file_put_contents(PDF_SAVE_LOCATION. $filename, $pdf))
+		{
+			print 'Could not save PDF';	
+		}
+	}
+  }
+
+
+?>
