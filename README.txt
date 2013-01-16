89cef81416d24ad900310888efcd31d669bcec6c
Blue Liquid Designs <Blue Liquid Designs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-16 06:02:56 +0000

    Version 2.0.0 Released.
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-pdf-extended/tags/2.0.0@653403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,103 @@
+=== Plugin Name ===
+Contributors: blueliquiddesigns
+Donate link: http://www.gravityformspdfextended.com
+Tags: gravity, forms, pdf, automation, attachment
+Requires at least: 3.4.1
+Tested up to: 3.5
+Stable tag: 2.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Gravity Forms PDF Extended allows you to save/view/download a PDF from the front- and back-end, and automate PDF creation on form submission. 
+
+== Description ==
+
+Gravity Forms PDF Extended is a plugin for Wordpress and Gravity Forms that allows your web server to create PDFs when a user submits a Gravity Form. Not only that, you can then easily attach the document to an email.
+
+At its core the plugin uses the power of the DOMPDF library to convert HTML and CSS into PDFs.
+
+**Features**
+
+* Save PDF File on user submission of a Gravity Form so it can be attached to a notification
+* Customise the PDF template without affecting the core Gravity Form Plugin
+* Multiple PDF Templates
+* Custom PDF Name
+* Output individual form fields in the template - like MERGETAGS.
+* View and download a PDF via the administrator interface
+* Simple function to output PDF via template / plugin
+* Works with Gravity Forms Signature Add-On
+* Installs a sample form using the new MERGETAGS-style template to help customisation
+
+** Server Requirements **
+1. PHP 5.0+ (5.3 recommended)
+2. MBString extension
+3. DOM extension (bundled with PHP 5)
+4. If you want images in your PDF you'll also need the GD Library
+
+** Software Requirements **
+
+1. [Purchase and install Gravity Forms](https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=235154)
+2. Wordpress 3.0+
+3. Gravity Forms 1.6.9+
+
+**Documentation and Support**
+To view the Development Documentation head to [http://www.gravityformspdfextended.com/documentation/](http://www.gravityformspdfextended.com/documentation/). If you need support with the plugin please post a topic in our [support forums](http://gravityformspdfextended.com/support/gravity-forms-pdf-extended/).
+
+== Installation ==
+
+1. Upload this plugin to your website and activate it
+2. Create a form in Gravity Forms and configure notifications
+3. Get the Form ID and follow the steps below in [the configuration section](http://gravityformspdfextended.com/documentation/2.0.0/1/configuration/)
+4. Modify the PDF template file ([see the advanced templating section in the documentation](http://gravityformspdfextended.com/documentation/2.0.0/9/advanced-configuration/)), pdf-print-entry.php or example-template.php, inside your active theme's PDF_EXTENDED_TEMPLATES/ folder.
+
+
+== Frequently Asked Questions ==
+
+All FAQs can be [viewed on the Gravity Forms PDF Extended website](http://gravityformspdfextended.com/faq/category/developers/).  
+
+== Screenshots ==
+
+1. View PDF from the Gravity Forms entries list.
+2. View or download the PDF from a Gravity Forms entry.
+
+== Changelog ==
+
+= 2.0.0 =
+* Moved templates to active theme folder to prevent custom themes being removed on upgrade
+* Allow PDFs to be saved using a custom name
+* Fixed WP_Error bug when image/css file cannot be found
+* Upgraded to latest version of DOMPDF
+* Removed auto-load form bug which would see multiple instances of the example form loaded
+* Created a number of constants to allow easier developer modification
+* Plugin/Support moved to dedicated website.
+* Pro/Business package offers the ability to write fields on an existing PDF.
+
+= 1.2.3 =
+* Fixed $wpdb->prepare error
+
+= 1.2.2 =
+* Fixed bug with tempalte shipping method MERGETAGS
+* Fixed bug where attachment wasn't being sent
+* Fixed problem when all_url_fopen was turned off on server and failed to retreive remote images. Now uses WP_HTTP class.
+
+= 1.2.1 =
+* Fixed path to custom css file included in PDF template 
+
+= 1.2.0 =
+* Template files moved to the plugin's template folder
+* Sample Form installed so developers have a working example to modify
+* Fixed bug when using WordPress in another directory to the site
+
+= 1.1.0 =
+* Now compatible with Gravity Forms Signature Add-On
+* Moved the field data functions out side of the Gravity Forms core so users can freely style their form information (located in pdf-entry-detail.php)
+* Simplified the field data output
+* Fixed bug when using product information
+
+= 1.0.0 =
+* First release. 
+
+== Upgrade Notice ==
+
+= 2.0.0 =
+New Features: Added custom PDF names and moved templates to active theme's folder (no longer overridden after updating). Also fixed a number of bugs in the problem. Remember to backup your custom templates before upgrading! 
---
+++ pdf.php
@@ -0,0 +1,297 @@
+<?php
+
+/*
+Plugin Name: Gravity Forms PDF Extended
+Plugin URI: http://www.gravityformspdfextended.com
+Description: Gravity Forms PDF Extended allows you to save/view/download a PDF from the front- and back-end, and automate PDF creation on form submission. Our Business Plus package also allows you to overlay field onto an existing PDF.
+Version: 2.0.0
+Author: Blue Liquid Designs
+Author URI: http://www.blueliquiddesigns.com.au
+
+------------------------------------------------------------------------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+*/
+
+/*
+ * Handles the admin area routing 
+ * See render_to_pdf.php for PDF Output Functions
+ */
+
+add_action('gform_entries_first_column_actions', 'pdf_link', 10, 4);
+add_action("gform_entry_info", "detail_pdf_link", 10, 2);
+add_action('wp',   'process_exterior_pages');
+register_activation_hook( __FILE__, 'pdf_extended_activate' );
+
+define('PDF_PLUGIN_DIR', ABSPATH. 'wp-content/plugins/gravity-forms-pdf-extended/');
+define('GF_PDF_SUPPORTED_VERSION', '1.6.0');
+
+add_action('admin_init',  'gfe_admin_init', 9);
+add_action("gform_entry_created", "gform_pdf_example_create", 15, 2);
+add_filter("gform_admin_notification_attachments", 'gform_add_example_attachment', 10, 3);
+add_filter("gform_user_notification_attachments", 'gform_add_example_attachment', 10, 3);
+
+/*
+ * Generate our Sample PDF for our Sample Form   
+ */
+function gform_pdf_example_create($entry, $form)
+{
+	$user_id = $entry['id'];
+	$form_id = $entry['form_id'];
+		
+	if($form['title'] == 'Gravity Forms PDF Extended Custom Template Sample')
+	{
+		/* include the pdf processing file */
+		require PDF_PLUGIN_DIR.'render_to_pdf.php';
+        /* generate and save the PDF using a custom name*/
+		$filename = PDF_Generator($form_id, $user_id, 'save', true, 'example-template.php');	
+	}
+}
+
+/* Emails our sample PDF to the site administrator */
+function gform_add_example_attachment($attachments, $lead, $form) {
+ 
+	$form_id = $lead['form_id'];
+	$user_id = $lead['id'];
+	$attachments = (is_array($attachments)) ? $attachments : array();
+	$folder_id = $form_id.$user_id.'/';
+ 
+	if($form['title'] == 'Gravity Forms PDF Extended Custom Template Sample')
+	{
+		/* include PDF converter plugin */
+		include PDF_PLUGIN_DIR.'render_to_pdf.php';
+		$attachment_file = PDF_SAVE_LOCATION. $folder_id . get_pdf_filename($form_id, $user_id);
+		$attachments[] = $attachment_file;
+		return $attachments;
+	}  
+}
+
+/**
+ * Check to see if Gravity Forms is actually installed
+ */
+function gfe_admin_init()
+{
+
+	if(!class_exists("RGForms"))
+	{ 
+		/* throw error to the admin notice bar */
+		add_action('admin_notices', 'gf_pdf_not_installed'); 
+		return;
+	}
+	
+	if(!gform_pdf_is_gravityforms_supported())
+	{
+		add_action('admin_notices', 'gf_pdf_not_supported'); 	
+		return;
+	}
+	
+	if(get_option('gf_pdf_extended_sample') != 'installed')
+	{
+		pdf_extended_activate();
+	}
+}
+
+/**
+ * Check to see if Gravity Forms version is supported
+ */
+function gform_pdf_is_gravityforms_supported(){
+	if(class_exists("GFCommon"))
+	{
+		$is_correct_version = version_compare(GFCommon::$version, GF_PDF_SUPPORTED_VERSION, ">=");
+		return $is_correct_version;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+/**
+ * Install an example form to show off the new template system
+ */
+function pdf_extended_activate()
+{
+	if(!class_exists("RGForms"))
+	{ 
+		/* throw error to the admin notice bar */
+		echo '<span style="font-family: Arial, sans-serif; font-size: 12px;">Gravity Forms needs to be installed to use this plugin. <a href="https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=235154" target="ejejcsingle">Purchase a copy of Gravity Forms now</a>.</span>';
+		exit;
+	}
+	
+	if(!gform_pdf_is_gravityforms_supported())
+	{
+		echo '<span style="font-family: Arial, sans-serif; font-size: 12px;">You need at least version 1.6.0 of Gravity Forms to use this plugin. <a href="https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=235154" target="ejejcsingle">Upgrade Gravity Forms now</a>.</span>';
+		exit;	
+	}	
+
+	/* preload example pdf form into database if it isn't installed already */
+	if(!RGFormsModel::get_form_id('Gravity Forms PDF Extended Custom Template Sample'))
+	{
+		GFExport::import_file(PDF_PLUGIN_DIR . 'example-form.xml');
+	}
+	
+	include PDF_PLUGIN_DIR.'render_to_pdf.php';
+	
+	/* create new directory in active themes folder*/	
+	if(!is_dir(PDF_TEMPLATE_LOCATION))
+	{
+		if(mkdir(PDF_TEMPLATE_LOCATION) === false)
+		{
+			add_action('admin_notices', 'gf_pdf_template_dir_err'); 	
+			return;
+		}
+	}
+
+	if(!is_dir(PDF_SAVE_LOCATION))
+	{
+		/* create new directory in active themes folder*/	
+		if(mkdir(PDF_SAVE_LOCATION) === false)
+		{
+			add_action('admin_notices', 'gf_pdf_template_dir_err'); 	
+			return;
+		}
+	}
+	
+	if(!file_exists(PDF_TEMPLATE_LOCATION.'pdf-print-entry.php'))
+	{
+		/* copy template files to new directory */
+		if(!copy(PDF_PLUGIN_DIR . 'templates/pdf-print-entry.php', PDF_TEMPLATE_LOCATION.'pdf-print-entry.php'))
+		{
+			add_action('admin_notices', 'gf_pdf_template_move_err'); 	
+			return;
+		}	
+	}
+	
+	if(!file_exists(PDF_TEMPLATE_LOCATION.'example-template.php'))
+	{
+		/* copy template files to new directory */
+		if(!copy(PDF_PLUGIN_DIR .'templates/example-template.php', PDF_TEMPLATE_LOCATION.'example-template.php'))
+		{
+			add_action('admin_notices', 'gf_pdf_template_move_err'); 	
+			return;
+		}
+	}
+	
+	if(!file_exists(PDF_TEMPLATE_LOCATION.'template.css'))
+	{
+		/* copy template files to new directory */
+		if(!copy(PDF_PLUGIN_DIR .'template.css', PDF_TEMPLATE_LOCATION.'template.css'))
+		{
+			add_action('admin_notices', 'gf_pdf_template_move_err'); 	
+			return;
+		}
+	}	
+
+	if(!file_exists(PDF_SAVE_LOCATION.'.htaccess'))
+	{		
+		if(!file_put_contents(PDF_SAVE_LOCATION.'.htaccess', 'deny from all'))
+		{
+			add_action('admin_notices', 'gf_pdf_template_move_err'); 	
+			return;
+		}	
+	}
+	
+	/* update system to ensure everything is installed correctly */
+	update_option('gf_pdf_extended_sample', 'installed');
+}
+
+/**
+ * Gravity Forms hasn't been installed so throw error.
+ * We make sure the user hasn't already dismissed the error
+ */
+function gf_pdf_not_installed()
+{
+	echo '<div id="message" class="error"><p>';
+	echo 'You need to install <a href="https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=235154" target="ejejcsingle">Gravity Forms</a> to use the Gravity Forms PDF Extended Plugin.';
+	echo '</p></div>';
+}
+
+/**
+ * The Gravity Forms version isn't compatible. Prompt user to upgrade
+ */
+function gf_pdf_not_supported()
+{
+		echo '<div id="message" class="error"><p>';
+		echo 'Gravity Forms PDF Extended only works with Gravity Forms version '.GF_PDF_SUPPORTED_VERSION.' and higher. Please <a href="https://www.e-junkie.com/ecom/gb.php?cl=54585&c=ib&aff=235154" target="ejejcsingle">upgrade your copy of Gravity Forms</a> to use this plugin.';
+		echo '</p></div>';	
+}
+							
+
+/**
+ * Cannot create new template folder in active theme directory
+ */
+function gf_pdf_template_dir_err()
+{
+		echo '<div id="message" class="error"><p>';
+		echo 'We could not create a template folder in your active theme\'s directory. Please created a folder called <strong>\'PDF_EXTENDED_TEMPLATES\'</strong> and place it in '.get_stylesheet_directory().'/. Then copy the contents of '.ABSPATH .'/wp-content/plugins/gravity-forms-pdf-extended/templates/ to your newly-created PDF_EXTENDED_TEMPLATES folder. You should also make this directory writable.';
+		echo '</p></div>';
+
+}
+
+/**
+ * Cannot create new template folder in active theme directory
+ */
+function gf_pdf_template_move_err()
+{
+		echo '<div id="message" class="error"><p>';
+		echo 'We could not copy the contents of '.ABSPATH .'/wp-content/plugins/gravity-forms-pdf-extended/templates/ to your newly-created PDF_EXTENDED_TEMPLATES folder. Please manually copy the files to the aforementioned directory. You should also make this directory writable.';
+		echo '</p></div>';
+
+}
+
+//Link for Entry Detail View (Provide both View Link and Download)
+function detail_pdf_link($form_id, $lead) {
+  $lead_id = $lead['id'];
+  echo "PDF:  ";
+  echo "<a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> View</a>";
+  echo " <a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&download=1&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> Download</a>";
+}
+
+// Made this first... figured i would leave it in.  View link on the Entry list view. 
+function pdf_link($form_id, $field_id, $value, $lead) {
+  $lead_id = $lead['id'];
+  echo "| <a href=\"javascript:;\" onclick=\"var notes_qs = '&notes=1'; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\"> View PDF</a>";
+}
+
+//Handle Incoming route.   Look for GF_PDF namespace 
+function process_exterior_pages(){
+  global $wpdb;
+  
+  if(rgempty("gf_pdf", $_GET))
+    return;
+    
+	$form_id = $_GET['fid'];
+	$lead_id = $_GET['lid'];
+	$ip = $_GET['ip'];
+	$template = (rgempty('template', $_GET)) ? 'pdf-print-entry.php' : rgget('template');
+	
+	/* check the lead is in the database and the IP address matches (little security booster) */
+	$form_entries = $wpdb->get_var( $wpdb->prepare("SELECT count(*) FROM `".$wpdb->prefix."rg_lead` WHERE form_id = ".$form_id." AND status = 'active' AND id = ".$lead_id." AND ip = '".$ip."'", array() ) )	;	
+
+  //ensure users are logged in
+  if(!is_user_logged_in() && !rgempty('template', $_GET) && $form_entries == 0)
+    auth_redirect();
+
+  switch(rgget("gf_pdf")){
+    case "print-entry" :
+	/* include the pdf processing file */
+	require PDF_PLUGIN_DIR.'render_to_pdf.php';
+	
+	/* call the creation class */
+	$output = ($_GET['download'] == 1) ? 'download' : 'view';
+	PDF_Generator((int) $_GET['fid'], (int) $_GET['lid'], $output, false, $template);
+    break;
+  }
+  exit();
+}
+
+?>
