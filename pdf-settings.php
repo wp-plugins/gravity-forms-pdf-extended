7bef081cd626d6b7169e98760c2bc413b3342052
Blue Liquid Designs <Blue Liquid Designs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-20 04:46:48 +0000

    2.2.0 Edition
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-pdf-extended/tags/2.2.0@684613 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pdf-settings.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Plugin: Gravity Forms PDF Extended
+ * File: settings.php
+ * 
+ * Handles the Gravity Forms Settings page in Wordpress
+ */
+
+class GFPDF_Settings
+{
+	/* 
+	 * Check if we're on the settings page 
+	 */ 
+	public function settings_page() {
+		if(RGForms::get("page") == "gf_settings") {
+
+			/* No update to templates this edition so no need to redeploy files */
+			/* Check if we need to redeploy default PDF templates/styles to the theme folder */
+			if( PDF_DEPLOY === true && rgpost("gfpdf_deploy") && rgpost('upgrade') && wp_verify_nonce($_POST['gfpdf_deploy_nonce'],'gfpdf_deploy_nonce_action')) {				
+				/* deploy new template styles */
+				self::deploy();
+			}
+					
+			/* Call settings page and */			
+			RGForms::add_settings_page("PDF", array("GFPDF_Settings", "gfpdf_settings_page"),'');
+		}			
+	}
+	
+	/*
+	 * Shows the GF PDF Extended settings page
+	 */		
+	public function gfpdf_settings_page() 
+	{ 
+		/* add deployment form */
+		?>
+        <div id="pdfextended-settings">
+                   
+                    <div class="leftcolumn">
+                     <h2><?php _e("Welcome to Gravity Forms PDF Extended v".PDF_EXTENDED_VERSION , "gravityformspdfextended") ?></h2>
+                     <h3>Freshly Installed?</h3>
+                     <p>Ensure you follow our <a href="http://gravityformspdfextended.com/documentation/introduction/">comprehensive installation guide</a> to get your website creating and emailing PDFs. If you run into any problems you can ask our friendly staff on our <a href="http://gravityformspdfextended.com/support/gravity-forms-pdf-extended/">support forums</a> or browse through the <a href="http://gravityformspdfextended.com/faq/category/developers/">FAQs</a>.</p>
+                         <h3>Just upgraded?</h3>
+                    <p>Changes have been made to Gravity Forms PDF Extended that may affect you. It's important to review these updates to ensure the plugin will continue working.</p>
+
+      <h3>Are you running Gravity Forms 1.6 or lower? </h3>
+      <p>If you're running Gravity Forms 1.6 you don't need to do a thing just yet. Once Gravity Forms 1.7 is released, and you upgrade, you'll need to modify the plugin to ensure compatibility. Until then, enjoy Gravity Forms PDF Extended.</p>
+      <h3>Upgraded to Gravity Forms 1.7? You'll need to address the following:</h3>
+        <ol>
+          <li> Gravity Forms 1.7 has overhauled the notification process and replaced a number of key hooks which Gravity Forms PDF Extended relied on. To ensure compatibility with new forms you will need to remove the two functions <em>gform_pdf_create</em> and <em>gform_add_attachment </em>and replace them with <strong>gfpdfe_create_and_attach_pdf()</strong>. <a href="http://gravityformspdfextended.com/gravity-forms-pdf-extended-2-2-0-release-notes/">Please review our Gravity Forms 1.7 upgrade guide</a> which has a detailed explanation on changes you need to make.        </li>
+        </ol>
+        <p>If you need assistance upgrading please don't hesitate to <a href="http://gravityformspdfextended.com/support/gravity-forms-pdf-extended/">create a topic in our support forum</a> or email us directly on enquire@blueliquiddesigns.com.au.</p>
+
+          <?php 
+		  	if(PDF_DEPLOY === true)
+			{
+				/* No updated to template files this edition so no need to redeploy */
+		   ?>
+		  	
+<form method="post">
+                <?php wp_nonce_field('gfpdf_deploy_nonce_action','gfpdf_deploy_nonce'); ?>
+                <input type="hidden" name="gfpdf_deploy" value="1">
+                <input type="submit" value="Deploy Templates" class="button" id="upgrade" name="upgrade">
+          </form>   
+          <?php } ?>     
+        
+        
+        </div>
+        <div class="rightcolumn">
+
+ 	<h2><?php _e('What\'s new in v'.PDF_EXTENDED_VERSION.'?' , "gravityformspdfextended") ?></h2>
+    
+          <p>A number of refinements have been added to this release to make the software easier to use. We've also ensured it will remain compatible with Gravity Forms 1.7 when it is publicly released.</p>
+            <ol>
+              <li><strong>Compatibility:</strong> Ensure compatibility with Gravity Forms 1.7. We&rsquo;ve      updated the functions.php code and remove <em>gform_user_notification_attachments</em> and <em>gform_admin_notification_attachments</em> hooks which <a href="http://www.gravityhelp.com/gravity-forms-v1-7-developer-notes/">are now      depreciated</a>.      Functions <em>gform_pdf_create</em> and <em>gform_add_attachment </em>have been      removed and replaced with <strong>gfpdfe_create_and_attach_pdf().</strong> <a href="http://gravityformspdfextended.com/gravity-forms-pdf-extended-2-2-0-release-notes/">See upgrade documentation for details</a>.</li>
+             <li><strong>Enhancement:</strong> Added deployment code switch so the template redeployment feature can be turned on and off. This release doesn't require redeployment.</li>
+              <li><strong>Enhancement: </strong><em>PDF_Generator()</em> variables were getting long and complex so the third variable is now an array      which will pass all the optional arguments. The new 1.7 compatible      functions.php code includes this method by default. For backwards      compatibility the function will still work with the variable structure prior      to 2.2.0.<strong></strong></li>
+              <li><strong>Bug: </strong>Fixed error generated by legacy code in the function <em>PDF_processing()</em> which is located      in <em>render_to_pdf.php</em>.<strong></strong></li>
+              <li><strong>Bug:</strong> Images and stylesheets will now try and be accessed with a local path instead of a URL. It fixes problem where some hosts were preventing read access from a URL. No template changes are required.</li>
+              
+            </ol>
+<p>Detailed documentation on the latest updates can be found at the official <a href="http://gravityformspdfextended.com/documentation/">Gravity Forms PDF Extended website</a>.</p>
+		</div>            
+
+</div>
+        <?php
+	}
+	
+	/*
+	 * Deploy the latest template files
+	 */
+	private function deploy()
+	{
+		GFPDF_InstallUpdater::pdf_extended_activate(true);
+		add_action('admin_notices', array("GFPDF_Settings", "gf_pdf_deploy_success")); 	
+	}
+	
+	public function gf_pdf_deploy_success() {
+			echo '<div id="message" class="updated"><p>';
+			echo 'You\'ve successfully deployed the Gravity Forms PDF Extended template files.';
+			echo '</p></div>';		
+	}
+}
+
+?>
---
+++ pdf.php
@@ -0,0 +1,223 @@
+<?php
+
+/*
+Plugin Name: Gravity Forms PDF Extended
+Plugin URI: http://www.gravityformspdfextended.com
+Description: Gravity Forms PDF Extended allows you to save/view/download a PDF from the front- and back-end, and automate PDF creation on form submission. Our Business Plus package also allows you to overlay field onto an existing PDF.
+Version: 2.2.0
+Author: Blue Liquid Designs
+Author URI: http://www.blueliquiddesigns.com.au
+
+------------------------------------------------------------------------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
+/*
+ * Define our constants 
+ */
+ if(!defined('PDF_EXTENDED_VERSION')) { define('PDF_EXTENDED_VERSION', '2.2.0'); }
+ if(!defined('PDF_PLUGIN_DIR')) { define('PDF_PLUGIN_DIR', plugin_dir_path( __FILE__ )); } 
+ if(!defined('PDF_PLUGIN_URL')) { define('PDF_PLUGIN_URL', plugin_dir_url( __FILE__ )); } 
+ if(!defined('GF_PDF_SUPPORTED_VERSION')) { define('GF_PDF_SUPPORTED_VERSION', '1.6.0'); }
+ if(!defined('PDF_SETTINGS_URL')) { define("PDF_SETTINGS_URL", site_url() .'/wp-admin/admin.php?page=gf_settings&addon=PDF'); }
+ if(!defined('PDF_SAVE_FOLDER')) { define('PDF_SAVE_FOLDER', 'PDF_EXTENDED_TEMPLATES'); }
+ if(!defined('PDF_SAVE_LOCATION')) { define('PDF_SAVE_LOCATION', get_stylesheet_directory().'/'.PDF_SAVE_FOLDER.'/output/'); }
+ if(!defined('PDF_TEMPLATE_LOCATION')) { define('PDF_TEMPLATE_LOCATION', get_stylesheet_directory().'/'.PDF_SAVE_FOLDER.'/'); }
+ if(!defined('PDF_TEMPLATE_URL_LOCATION')) { define('PDF_TEMPLATE_URL_LOCATION', get_stylesheet_directory_uri().'/'. PDF_SAVE_FOLDER .'/'); } 
+
+ /*
+  * Do we need to deploy template files this edition? If yes set to true. 
+  */
+  if(!defined('PDF_DEPLOY')) { define('PDF_DEPLOY', false); } 
+
+/* 
+ * Include the core files
+ */
+ include PDF_PLUGIN_DIR . 'installation-update-manager.php';
+ include PDF_PLUGIN_DIR . 'pdf-settings.php';
+
+/*
+ * Add our hooks
+ */
+add_action('admin_init',  array('GFPDF_Core', 'gfe_admin_init'), 9);
+add_filter("gform_notification", array('GFPDF_Core', 'gfpdfe_create_and_attach_pdf'), 999, 3);
+add_action('after_switch_theme', array('GFPDF_InstallUpdater', 'gf_pdf_on_switch_theme'), 10, 2);
+add_action('gform_entries_first_column_actions', array('GFPDF_Core', 'pdf_link'), 10, 4);
+add_action("gform_entry_info", array('GFPDF_Core', 'detail_pdf_link'), 10, 2);
+add_action('wp', array('GFPDF_Core', 'process_exterior_pages'));
+register_activation_hook( __FILE__, array('GFPDF_InstallUpdater', 'install') );
+
+class GFPDF_Core
+{
+	function gfpdfe_create_and_attach_pdf($notification, $form, $entry)
+	{
+		$form_title = $form['title'];
+
+		if ($form_title == 'Gravity Forms PDF Extended Custom Template Sample') {		
+			$template_name = 'example-template.php';
+			require PDF_PLUGIN_DIR . 'render_to_pdf.php'; 			
+			$form_id           = $entry['form_id'];
+			$user_id           = $entry['id'];
+			$folder_id = $form_id.$user_id.'/';
+			$PDF_name          = get_pdf_filename($form_id, $user_id);
+			$pdf_arguments     = array(
+				'pdfname' => $PDF_name,
+				'template' => $template_name,
+				'return' => true
+			);	
+			
+			/* generate and save default PDF */
+			$filename = PDF_Generator($form_id, $user_id, $pdf_arguments);
+			$attachment_file               = PDF_SAVE_LOCATION . $folder_id . $PDF_name;
+			$notification['attachments'][] = $attachment_file;
+		}
+		return $notification;
+	}	
+	
+	/**
+	 * Check to see if Gravity Forms is actually installed
+	 */
+	function gfe_admin_init()
+	{	
+		/*
+		 * Check if Gravity Forms is installed 
+		 */
+		if(!class_exists("RGForms"))
+		{ 
+			/* throw error to the admin notice bar */
+			add_action('admin_notices', array("GFPDF_InstallUpdater", "gf_pdf_not_installed")); 
+			return;
+		}
+		
+		/*
+		 * Check if current version of Gravity Forms is supported
+		 */	
+		if(!GFPDF_InstallUpdater::gform_pdf_is_gravityforms_supported())
+		{
+			add_action('admin_notices', array("GFPDF_InstallUpdater", "gf_pdf_not_supported")); 	
+			return;
+		}
+		
+		/*
+		 * Check if GF PDF Extended is correctly installed. If not we'll run the installer.
+		 */	
+		if( (get_option('gf_pdf_extended_installed') != 'installed') || (get_option('gf_pdf_extended_version') != PDF_EXTENDED_VERSION) || (!is_dir(PDF_TEMPLATE_LOCATION)) )
+		{		
+			GFPDF_InstallUpdater::pdf_extended_activate();
+		}
+		
+		/* 
+		 * Check if database plugin version matches current plugin version and updates if needed
+		 */
+		if(get_option('gf_pdf_extended_version') != PDF_EXTENDED_VERSION)
+		{
+			update_option('gf_pdf_extended_version', PDF_EXTENDED_VERSION);
+			/* redirect */
+			Header('Location: '.PDF_SETTINGS_URL);
+		}
+		
+		/**
+		 * Check if deployed new template files after update
+		 */ 
+		 if(get_option('gf_pdf_extended_deploy') == 'no' && !rgpost('upgrade') && PDF_DEPLOY === true) {
+			/*show warning message */
+			add_action('admin_notices', array("GFPDF_InstallUpdater", "gf_pdf_not_deployed")); 	
+		 }		
+		 
+		/*
+		 * Check if user is now running GF1.7
+		 */	
+		if(GFCommon::$version == '1.7')
+		{
+			add_action('admin_notices', array("GFPDF_InstallUpdater", "gf_pdf_now_1_7")); 		
+		}
+				 				
+		/* 
+		 * Configure the settings page
+		 */
+		wp_enqueue_style( 'pdfextended-admin-styles', PDF_PLUGIN_URL . 'styles/admin-styles.css' );		 
+		 
+    	 GFPDF_Settings::settings_page();		
+	}
+	
+	
+	function detail_pdf_link($form_id, $lead) {
+	  $lead_id = $lead['id'];
+	
+	  echo "PDF:  ";
+	  echo "<a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> View</a>";
+	  echo " <a href=\"javascript:;\" onclick=\"var notes_qs = jQuery('#gform_print_notes').is(':checked') ? '&notes=1' : ''; var url='".home_url()."/?gf_pdf=print-entry&download=1&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\" class=\"button\"> Download</a>";
+	}
+	
+	function pdf_link($form_id, $field_id, $value, $lead) {
+	  $lead_id = $lead['id'];
+	  echo "| <a href=\"javascript:;\" onclick=\"var notes_qs = '&notes=1'; var url='".home_url()."/?gf_pdf=print-entry&fid=".$form_id."&lid=".$lead_id."' + notes_qs; window.open (url,'printwindow');\"> View PDF</a>";
+	}
+	
+	//Handle Incoming route.   Look for GF_PDF namespace 
+	function process_exterior_pages() {
+	  global $wpdb;
+	  
+	  if(rgempty("gf_pdf", $_GET))
+		return;
+		
+		$form_id = (int) $_GET['fid'];
+		$lead_id = (int) $_GET['lid'];
+		$ip = self::getRealIpAddr();
+		$template = (rgempty('template', $_GET)) ? 'default-template.php' : rgget('template');
+		
+		/* check the lead is in the database and the IP address matches (little security booster) */
+		$form_entries = $wpdb->get_var( $wpdb->prepare("SELECT count(*) FROM `".$wpdb->prefix."rg_lead` WHERE form_id = ".$form_id." AND status = 'active' AND id = ".$lead_id." AND ip = '".$ip."'", array() ) )	;	
+	
+	  //ensure users are logged in
+	  if( !is_user_logged_in() && $form_entries == 0) {
+		auth_redirect();
+	  }
+	  
+	  // ensure logged in users have the correct privliages
+	  if(!GFCommon::current_user_can_any("gravityforms_view_entries") && $form_entries == 0)
+	  {
+		 break;  
+	  }
+	
+	  switch(rgget("gf_pdf")){
+		case "print-entry" :
+		/* include the pdf processing file */
+		require PDF_PLUGIN_DIR.'render_to_pdf.php';
+		
+		/* call the creation class */
+		$output = ($_GET['download']) ? 'download' : 'view';
+		PDF_Generator((int) $_GET['fid'], (int) $_GET['lid'], $output, false, $template);
+		break;
+	  }
+	  exit();
+	}
+	
+	public function getRealIpAddr()
+	{
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+		{
+		  $ip=$_SERVER['HTTP_CLIENT_IP'];
+		}
+		elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+		{
+		  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+		else
+		{
+		  $ip=$_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}	
+}
+
+?>
